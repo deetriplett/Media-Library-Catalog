4b3fb6c6aca7f2302f53a48ea3ba91983fc27f7c
deetriplett <deetriplett@gmail.com>
2016-06-04 10:39:03 -0400

    Added class "items" to ul

--- catalog.php
+++ catalog.php
@@ -22,7 +22,7 @@ include("inc/header.php"); ?>
 <div class="section catalog page">
   <div class="wrapper">
     <h1><?php echo $pageTitle; ?></h1>
-    <ul>
+  <ul class="items">
       <?php foreach($catalog as $item){
         echo "<li>" . $item . "</li>";
       }
