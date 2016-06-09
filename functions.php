8d2033623274cbadb913a1d19b37954799dd28b0
deetriplett <deetriplett@gmail.com>
2016-06-09 07:40:37 -0700

    added function array_category and catalog loop sorting by title

--- functions.php
+++ functions.php
@@ -9,7 +9,24 @@ function get_item_html($id, $item) {
 
 }
 
+function array_category($catalog, $category){
+  if($catgeory == null){
+    return array_keys($catalog);}
+  $output = array();
+  
+  foreach($catalog as $id => $item){
+    if(strtolower($category) == strtolower($item[$category])){
+      $sort = $item["title"]; //sorts by title
+      $sort = ltrim($sort, "The ");
+      $sort = ltrim($sort, "A ");
+      $sort = ltrim($sort, "An ");//trim form the left, targets "the", etc. 
+      $output[$id] = $sort; 
+    }
+  }//end foreach loop
+  asort($output);
+  return array_keys($output);
+  
 
-
+}
 
 
