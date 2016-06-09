1f4b1bd8bc19bd54196259d60c90fa2ed5ea2b0a
deetriplett <deetriplett@gmail.com>
2016-06-09 07:25:14 -0700

    Amended $random to $categories and created an array_category

--- catalog.php
+++ catalog.php
@@ -24,8 +24,8 @@ include("inc/header.php"); ?>
     <h1><?php echo $pageTitle; ?></h1>
     <ul class="items">
       <?php 
-         $random = array_rand($catalog, 4);
-          foreach($random as $id){
+         $categories = array_category($catalog, $section);
+          foreach($categories as $id){
           echo get_item_html($id, $catalog[$id]);
         }
       ?>
