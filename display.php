847bea68c11221c799f0d9be611aad63de41755e
deetriplett <deetriplett@gmail.com>
2016-06-09 08:23:32 -0700

    Display the details/data of each item

---
+++ display.php
@@ -0,0 +1,33 @@
+<?php 
+include("inc/data.php");
+include("inc/functions.php");
+
+
+if (isset($_GET["id"])) {
+ $id = $_GET["id"];
+  if(isset($catalog[$id])){
+    $item = $catalog[$id];
+  }
+}
+
+if (!isset($item)){
+  header("location: catalog.php");//redirect, if item not set look in a different location
+  exit;
+}
+
+$pageTitle = $item["title"];
+$section = null;
+
+include("inc/header.php"); ?>
+
+<div class="section page">
+  <div class="wrapper">
+    <img src="" alt="" />
+  </div>
+</div>
+
+
+<?php include("inc/footer.php"); ?>
+
+
+
