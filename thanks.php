d54906e94e25c9d22792c1cbbde77068156383bb
deetriplett <deetriplett@gmail.com>
2016-06-11 17:19:38 -0700

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Thank You!";
+$section = null;
+
+<div class="section page">
+  <h1>Thank You!</h1>
+  <p>Thanks for the order: I &rsquo;ll check out your order shortly!</p>
+</div>
+    
+include("inc/footer.php"); 
